3d07906cfbb55e9de0a466cb679b51fa6177ebca
JSIUVippsMobilepay <jonas.siuksteris@vippsmobilepay.com>
2024-01-07 23:05:58 +0200

    Add calculator example

--- Calculator/v3/calculator.go
+++ Calculator/v3/calculator.go
@@ -1,48 +1,78 @@
-package v3
+package main
 
 import (
-	"errors"
 	"fmt"
 )
 
+// Operation interface for different arithmetic operations
+type Operation interface {
+	Execute(a, b int) (int, error)
+}
+
+// AddOperation struct for addition
+type AddOperation struct{}
+
+func (op AddOperation) Execute(a, b int) (int, error) {
+	return a + b, nil
+}
+
+// SubtractOperation struct for subtraction
+type SubtractOperation struct{}
+
+func (op SubtractOperation) Execute(a, b int) (int, error) {
+	return a - b, nil
+}
+
+// MultiplyOperation struct for multiplication
+type MultiplyOperation struct{}
+
+func (op MultiplyOperation) Execute(a, b int) (int, error) {
+	return a * b, nil
+}
+
+// AdvancedCalculator struct that uses Operation interface
 type AdvancedCalculator struct {
-	total     int
-	operation string
+	operation Operation
 }
 
-func (ac *AdvancedCalculator) add(a, b int) {
-	ac.total = a + b
-	ac.operation = "add"
+func (ac *AdvancedCalculator) SetOperation(op Operation) {
+	ac.operation = op
 }
 
-func (ac *AdvancedCalculator) subtract(a, b int) {
-	ac.total = a - b
-	ac.operation = "subtract"
-}
-
-func (ac *AdvancedCalculator) multiply(a, b int) {
-	ac.total = a * b
-	ac.operation = "multiply"
-}
-
-func (ac *AdvancedCalculator) displayResult() error {
-	if ac.operation == "" {
-		return errors.New("no operation performed")
+func (ac *AdvancedCalculator) Calculate(a, b int) (int, error) {
+	if ac.operation == nil {
+		return 0, fmt.Errorf("no operation set")
 	}
-	fmt.Printf("Result after %s: %d\n", ac.operation, ac.total)
-	return nil
+	return ac.operation.Execute(a, b)
 }
 
 func main() {
 	calc := AdvancedCalculator{}
-	calc.add(10, 5)
-	err := calc.displayResult()
+
+	// Addition
+	calc.SetOperation(AddOperation{})
+	result, err := calc.Calculate(10, 5)
 	if err != nil {
 		fmt.Println("Error:", err)
+	} else {
+		fmt.Println("Addition Result:", result)
 	}
-	calc.multiply(3, 3)
-	err = calc.displayResult()
+
+	// Subtraction
+	calc.SetOperation(SubtractOperation{})
+	result, err = calc.Calculate(10, 5)
 	if err != nil {
 		fmt.Println("Error:", err)
+	} else {
+		fmt.Println("Subtraction Result:", result)
+	}
+
+	// Multiplication
+	calc.SetOperation(MultiplyOperation{})
+	result, err = calc.Calculate(10, 5)
+	if err != nil {
+		fmt.Println("Error:", err)
+	} else {
+		fmt.Println("Multiplication Result:", result)
 	}
 }
